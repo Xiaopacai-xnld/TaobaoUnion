bef8a4ee9c8a1037f5e9dff88e943698dbb3eb48
王浩 <haowang@tinnove.com.cn>
2025-09-26 18:48:36 +0800

    second commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TaobaoUnion
 领券联盟
+
+[![](https://jitpack.io/v/Xiaopacai-xnld/TaobaoUnion.svg)](https://jitpack.io/#Xiaopacai-xnld/TaobaoUnion)
--- app/src/main/java/com/example/taobaounion/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/taobaounion/ui/activity/MainActivity.java
@@ -1,26 +1,70 @@
 package com.example.taobaounion.ui.activity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.taobaounion.R;
+import com.example.taobaounion.databinding.ActivityMainBinding;
+import com.example.taobaounion.ui.fragment.HomeFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        initView();
+        initListener();
+    }
+
+    private void initListener() {
+        binding.mainNavigationBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Log.d(TAG, "title -- > " + item.getTitle() + "id -- > " + item.getItemId());
+                if (item.getItemId() == R.id.home) {
+                    Log.d(TAG, "切换到首页");
+
+                } else if (item.getItemId() == R.id.selected) {
+                    Log.d(TAG, "切换到精选");
+
+                } else if (item.getItemId() == R.id.red_packet) {
+                    Log.d(TAG, "切换到特惠");
+
+                } else if (item.getItemId() == R.id.search) {
+                    Log.d(TAG, "切换到搜索");
+
+                }
+                return true;
+            }
         });
+    }
+
+    private void initView() {
+        HomeFragment homeFragment = new HomeFragment();
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        transaction.add(binding.mainPageContainer.getId(), homeFragment);
+        transaction.commit();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy----- ");
+        super.onDestroy();
+
+        binding = null;
     }
 }
