8fa8f75478fd17355773cdea0fa79307cdc54814
王浩 <haowang@tinnove.com.cn>
2025-10-26 20:56:18 +0800

    10_26

--- app/src/main/java/com/example/taobaounion/ui/fragment/HomeFragment.java
+++ app/src/main/java/com/example/taobaounion/ui/fragment/HomeFragment.java
@@ -2,21 +2,22 @@ package com.example.taobaounion.ui.fragment;
 
 
 import android.view.View;
-import android.widget.TableLayout;
 
 import com.example.taobaounion.R;
 import com.example.taobaounion.base.BaseFragment;
+import com.example.taobaounion.databinding.FragmentHomeBinding;
 import com.example.taobaounion.model.domain.Categories;
 import com.example.taobaounion.presenter.impl.HomePresenterImpl;
+import com.example.taobaounion.ui.adapter.HomePagerAdapter;
+import com.example.taobaounion.utils.LogUtils;
 import com.example.taobaounion.view.IHomeCallback;
-
-import butterknife.BindView;
 
 public class HomeFragment extends BaseFragment implements IHomeCallback {
 
-    @BindView(R.id.home_indicator)
-    private TableLayout mTableLayout;
+    private FragmentHomeBinding binding;
     private HomePresenterImpl mHomePresenter;
+    private HomePagerAdapter mhomePagerAdapter;
+
 
     @Override
     protected int getRootViewResId() {
@@ -25,7 +26,14 @@ public class HomeFragment extends BaseFragment implements IHomeCallback {
 
     @Override
     protected void initView(View rootView) {
-        mTableLayout.setupWithViewPager();
+        binding = FragmentHomeBinding.bind(rootView);
+
+        binding.homeIndicator.setupWithViewPager(binding.homePager);
+
+        //给ViewPager设置适配器
+        mhomePagerAdapter = new HomePagerAdapter(getChildFragmentManager());
+        //设置适配器
+        binding.homePager.setAdapter(mhomePagerAdapter);
     }
 
     @Override
@@ -43,8 +51,11 @@ public class HomeFragment extends BaseFragment implements IHomeCallback {
 
     @Override
     public void onCategoriesLoaded(Categories categories) {
+        LogUtils.d(this, "onCategoriesLoaded...");
         //加载的数据就会从这里回来
-
+        if (mhomePagerAdapter != null) {
+            mhomePagerAdapter.setCategories(categories);
+        }
     }
 
     @Override
